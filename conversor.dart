9512c4c58221d18ba8f56313c3ac11034201c797
DosonsVitor <64105907+DosonsVitor@users.noreply.github.com>
2022-05-03 13:56:41 -0300

    Update conversor.dart

--- conversor.dart
+++ conversor.dart
@@ -1,13 +1,20 @@
 void main(){
-    int dec = 10;
+    int dec = 79;
     List<int> bin = [0];
-    print(bin);
-    for (var i = 0; i < 10; i++) {
-      for(var a = 0; a < bin.length; a++){
+    for (var i = 0; i < dec; i++) {
+      for(var a = 1; a <= bin.length; a++){
         if(bin[bin.length-a] == 0){
           bin[bin.length-a] = 1;
+          bin.insert(0, 0);
           break;
+        }
+        else{
+          bin[bin.length-a] = 0;
         }
       }
     }
+
+    while (bin[0]==0) { bin.removeAt(0); }
+
+    print(bin);
 }
